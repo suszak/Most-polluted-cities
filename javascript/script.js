5c0396cfc4f07833a1e9803a156216b1c9849b88
suszak <44030399+suszak@users.noreply.github.com>
2019-10-29 23:01:30 +0100

    small changes in header, input styled

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,23 @@
-document.querySelector("#city").addEventListener("click", () => {
-    document.querySelector(".choice__label").classList.add("choice__label--active");
-    document.querySelector(".choice__input").classList.add("choice__input--active");
+var inputFlag = 0;
+
+window.addEventListener('DOMContentLoaded', () => {
+    
+    document.querySelector("#country").addEventListener("click", (e) => {
+        if (inputFlag === 0) {
+            document.querySelector(".choice__label").classList.add("choice__label--active");
+            document.querySelector(".choice__input").classList.add("choice__input--active");
+            inputFlag = 1;
+            console.log("active");
+        }
+    });
+
+    document.addEventListener("click", (e) => {
+        if (inputFlag === 1 && document.querySelector("#country").value === "" && e.target.id !== "country") {
+            document.querySelector(".choice__label").classList.remove("choice__label--active");
+            document.querySelector(".choice__input").classList.remove("choice__input--active");
+            inputFlag = 0;
+            console.log(e.target.id);
+        }
+    })
+
 });
