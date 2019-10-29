05ab651c42001b41ad9903607b4994a123d021ea
suszak <44030399+suszak@users.noreply.github.com>
2019-10-29 15:37:59 +0100

    header section finished

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#city").addEventListener("click", () => {
+    document.querySelector(".choice__label").classList.add("choice__label--active");
+    document.querySelector(".choice__input").classList.add("choice__input--active");
+});
