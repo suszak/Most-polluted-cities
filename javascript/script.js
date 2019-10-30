fbd5be4525fcad937897097d9910626e43fed854
suszak <44030399+suszak@users.noreply.github.com>
2019-10-30 15:20:16 +0100

    input verifier now works

--- javascript/script.js
+++ javascript/script.js
@@ -1,31 +1,51 @@
-let inputFlag = 0;
+let inputFlag = 0; // input active or not
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// window.addEventListener('resize', () => {
-//     let vh = window.innerHeight * 0.01;
-//     document.documentElement.style.setProperty('--vh', `${vh}px`);
-// });
+// array with countries:
+const countriesArray = ["POLAND", "GERMANY", "SPAIN", "FRANCE"];
 
 window.addEventListener('DOMContentLoaded', () => {
     
+    // header section events:
+    document.querySelector("#goNav").addEventListener("click", () => {
+        document.querySelector("#navigation").scrollIntoView();
+    });
+
+    // navigation section events:
+    // nav: input enabling:
     document.querySelector("#country").addEventListener("click", (e) => {
         if (inputFlag === 0) {
             document.querySelector(".choice__label").classList.add("choice__label--active");
             document.querySelector(".choice__input").classList.add("choice__input--active");
             inputFlag = 1;
-            console.log("active");
+            // console.log("active");
         }
     });
 
+
+    // nav: verifier change:
+    document.querySelector("#country").addEventListener("keyup", (e) => {
+        
+            if (countriesArray.indexOf(document.querySelector("#country").value.toUpperCase()) !== -1) {
+                document.querySelector("#verifier").classList.remove("fa-times");
+                document.querySelector("#verifier").classList.add("fa-check");
+            } else {
+                document.querySelector("#verifier").classList.add("fa-times");
+                document.querySelector("#verifier").classList.remove("fa-check");
+            }
+        
+    });
+
+    // nav: input disabling:
     document.addEventListener("click", (e) => {
+
         if (inputFlag === 1 && document.querySelector("#country").value === "" && e.target.id !== "country") {
             document.querySelector(".choice__label").classList.remove("choice__label--active");
             document.querySelector(".choice__input").classList.remove("choice__input--active");
-            inputFlag = 0;
-            console.log(e.target.id);
+            inputFlag = 0; 
         }
-    })
+    });
 
 });
